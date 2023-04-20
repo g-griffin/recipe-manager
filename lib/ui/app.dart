53b2416e52579c95fccef169efc7ecf93ca7da5b
g-griffin <gretchen.griffin@gmx.de>
2023-04-20 11:13:17 +0200

    Add basic Bottom Navigation

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_manager/constants/strings.dart';
 import 'package:recipe_manager/ui/login.dart';
+import 'package:recipe_manager/utils/nav_bar_handler.dart';
 import 'package:recipe_manager/utils/routes.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: Strings.appName,
       theme: ThemeData(primarySwatch: Colors.blue),
       routes: Routes.routes,
-      home: const LoginScreen(),
+      home: const NavBarHandler(),
     );
   }
 }
---
+++ lib/utils/nav_bar_handler.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_manager/ui/search.dart';
+import 'package:recipe_manager/ui/home.dart';
+import 'package:recipe_manager/ui/scan.dart';
+
+class NavBarHandler extends StatefulWidget {
+  const NavBarHandler({Key? key}) : super(key: key);
+
+  @override
+  State<NavBarHandler> createState() => _NavBarHandlerState();
+}
+
+class _NavBarHandlerState extends State<NavBarHandler> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    HomeScreen(),
+    ScanScreen(),
+    SearchScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onIconTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: "Scan",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onIconTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
