14e0c1a2bf37999aacbb291808d1583293651f48
g-griffin <gretchen.griffin@gmx.de>
2023-04-24 18:28:11 +0200

    Add camera permission request & show camera preview on Scan tab

--- lib/ui/scan.dart
+++ lib/ui/scan.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({Key? key}) : super(key: key);
@@ -8,10 +10,117 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> with WidgetsBindingObserver {
+  bool _isPermissionGranted = false;
+
+  late final Future<void> _future;
+
+  CameraController? _cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    _future = _requestCameraPermission();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Scan recipes here"),
+    return FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          return Stack(
+            children: [
+              // Show the camera feed behind everything
+              if (_isPermissionGranted)
+                FutureBuilder<List<CameraDescription>>(
+                  future: availableCameras(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      _initCameraController(snapshot.data!);
+                      return Center(
+                        child: CameraPreview(_cameraController!),
+                      );
+                    } else {
+                      return const LinearProgressIndicator();
+                    }
+                  },
+                ),
+              Scaffold(
+                appBar: AppBar(
+                  title: const Text('Text Recognition Sample'),
+                ),
+                backgroundColor:
+                    _isPermissionGranted ? Colors.transparent : null,
+                body: _isPermissionGranted
+                    ? Column(children: [
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(bottom: 30.0),
+                            child: const Center(
+                              child: ElevatedButton(
+                                onPressed: null,
+                                child: Text('Scan text'),
+                              ),
+                            ))
+                      ])
+                    : Center(
+                        child: Container(
+                          padding:
+                              const EdgeInsets.only(left: 24.0, right: 24.0),
+                          child: const Text(
+                            'Camera permission denied',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> _requestCameraPermission() async {
+    final status = await Permission.camera.request();
+    _isPermissionGranted = status == PermissionStatus.granted;
+  }
+
+  void _initCameraController(List<CameraDescription> cameras) {
+    if (_cameraController != null) {
+      return;
+    }
+    CameraDescription? rearCamera;
+    for (var camera in cameras) {
+      if (camera.lensDirection == CameraLensDirection.back) {
+        rearCamera = camera;
+        break;
+      }
+    }
+    if (rearCamera != null) {
+      _cameraSelected(rearCamera);
+    }
+  }
+
+  Future<void> _cameraSelected(CameraDescription camera) async {
+    _cameraController = CameraController(
+      camera,
+      ResolutionPreset.max,
+      enableAudio: false,
     );
+
+    await _cameraController?.initialize();
+
+    if (!mounted) {
+      return;
+    }
+    setState(() {});
   }
 }
