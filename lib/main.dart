2cafe9d93b7646c9050102e86e92ee614c1980f7
g-griffin <gretchen.griffin@gmx.de>
2023-06-14 22:22:08 +0200

    Upgrade Flutter & Fix Flutter bindings Exception

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:recipe_manager/di/service_locator.dart';
-import 'package:recipe_manager/stores/recipe_index_store.dart';
 import 'package:recipe_manager/ui/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setupServiceLocator();
-  await serviceLocator<RecipeIndexStore>().loadRecipeIndices();
-
   return runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await setupServiceLocator();
     runApp(MyApp());
   }, (error, stack) {
     print(stack);
